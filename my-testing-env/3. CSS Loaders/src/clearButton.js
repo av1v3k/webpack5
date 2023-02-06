d0f4e13468da03665f7eadd53bf624a32cbb7b02
vanan07 <Vivek.Ananth@albertsons.com>
2023-02-06 17:39:57 +0530

    css modules

--- my-testing-env/3. CSS Loaders/src/clearButton.js
+++ my-testing-env/3. CSS Loaders/src/clearButton.js
@@ -1,5 +1,6 @@
 import "./clearButton.css";
-
+import style from "./clearButton.css";
+console.log(style);
 let btn = document.createElement("button");
 btn.innerHTML = "Clear";
 btn.classList.add("button");
